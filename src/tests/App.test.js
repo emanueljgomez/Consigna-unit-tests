e1cd660ed2c789c72e3cc0292a9a961a8d8a888a
Emanuel Gomez <emanueljgomez@gmail.com>
2021-09-13 12:31:07 -0300

    Added proper commentary for Test Case A

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,13 +5,18 @@ import App from "../App.js";
 describe("Test suite Cartas obtener usuarios", () => {
   let wrapper;
 
+  // 'App' component is assigned to 'wrapper' variable,
+  // using shallow as its rendering method, this is
+  // executed before each Test Case is run.
   beforeEach(() => {
     wrapper = shallow(<App />);
   });
 
-  // Test Case 'a'
+  // Test Case 'a':
+  // 'wrapper' was assigned the 'App' component, so here the
+  // 'toMatchSnapshot' method is used to make sure its content matches
+  // the content of its snapshot (App.test.js.snap)
   test("debe coincidir con snapshot", () => {
-    //const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
 });
