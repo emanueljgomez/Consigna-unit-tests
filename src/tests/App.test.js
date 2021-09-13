2677dbe69ce8208ed51127b10e8aa697aad9a714
Emanuel Gomez <emanueljgomez@gmail.com>
2021-09-13 12:25:24 -0300

    Test Case 'a' passed

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-/** TODO */
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../App.js";
+
+describe("Test suite Cartas obtener usuarios", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  // Test Case 'a'
+  test("debe coincidir con snapshot", () => {
+    //const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
